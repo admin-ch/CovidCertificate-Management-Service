cfa58047000c9537e4570b3f8b12a83157c19c46
Mathias Wegmüller <matthias.wegmueller@bit.admin.ch>
2021-06-01 22:26:49 +0200

    VACCINECER-442 Changed automated JSON file generation for EU tests to process multiple files.

--- src/test/java/ch/admin/bag/covidcertificate/DGCTestJSONGenerator.java
+++ src/test/java/ch/admin/bag/covidcertificate/DGCTestJSONGenerator.java
@@ -29,44 +29,38 @@ import java.nio.file.Files;
 import java.time.Clock;
 import java.time.OffsetDateTime;
 import java.time.ZoneId;
+import java.util.Objects;
 
 @Ignore
 @ExtendWith(MockitoExtension.class)
 public class DGCTestJSONGenerator {
-    private static final CertificateType TYPE = CertificateType.Vaccination;
+    private static final String PATH = "target/dgc-testdata/";
+    private static final String PNG = ".png";
+    private static final String JSON = ".json";
+    private static final Integer VERSION = 1;
+    private static final String JSON_SCHEMA = "1.0.0";
+    private static final String CERTIFICATE = "TODO: Replace with public key.";
+    private static final String ZONE_ID = "Europe/Zurich";
+    private static final String DESCRIPTION = "VALID ";
+    private static final String PREFIX = "HC1:";
+    private static final String IDENTIFIER_VACCINATION = "\"v\":";
+    private static final String IDENTIFIER_TEST = "\"t\":";
+    private static final String IDENTIFIER_RECOVERY = "\"r\":";
 
     @Test
-    public void createJSON() throws Exception {
-        // Setting the test context.
-        DGCTestJSONTestContext dgcTestJSONTestContext = new DGCTestJSONTestContext();
-        dgcTestJSONTestContext.setVersion(1);
-        dgcTestJSONTestContext.setJsonSchema("1.0.0");
-        dgcTestJSONTestContext.setCertificate("TODO: Replace with public key.");
-        dgcTestJSONTestContext.setValidationClock(OffsetDateTime.now(Clock.system(ZoneId.of("Europe/Zurich"))).withNano(0).toString());
-        dgcTestJSONTestContext.setDescription(String.format("VALID %s", TYPE));
+    public void createJSONFiles() throws Exception {
+        for (String qrCodeFilename : Objects.requireNonNull(new File(PATH).list())) {
+            if (qrCodeFilename.endsWith(PNG)) {
+                createJSONFile(qrCodeFilename);
+            }
+        }
+    }
 
-        // Setting the expected results.
-        DGCTestJSONExpectedResults dgcTestJSONExpectedResults = new DGCTestJSONExpectedResults();
-        dgcTestJSONExpectedResults.setExpectedValidObject(true);
-        dgcTestJSONExpectedResults.setExpectedSchemaValidation(true);
-        dgcTestJSONExpectedResults.setExpectedEncode(true);
-        dgcTestJSONExpectedResults.setExpectedDecode(true);
-        dgcTestJSONExpectedResults.setExpectedVerify(true);
-        dgcTestJSONExpectedResults.setExpectedCompression(true);
-        dgcTestJSONExpectedResults.setExpectedKeyUsage(true);
-        dgcTestJSONExpectedResults.setExpectedUnPrefix(true);
-        dgcTestJSONExpectedResults.setExpectedValidJSON(true);
-        dgcTestJSONExpectedResults.setExpectedB45Decode(true);
-        dgcTestJSONExpectedResults.setExpectedPictureDecode(true);
-        dgcTestJSONExpectedResults.setExpectedExpirationCheck(true);
-
-        // Setting the JSON.
+    private void createJSONFile(String qrCodeFilename) throws Exception {
         DGCTestJSON dgcTestJSON = new DGCTestJSON();
-        dgcTestJSON.setTestContext(dgcTestJSONTestContext);
-        dgcTestJSON.setExpectedResults(dgcTestJSONExpectedResults);
 
         // Getting the image as base64 string.
-        File qrCodeFile = new File("target/dgc-testdata/1.png");
+        File qrCodeFile = new File(PATH + qrCodeFilename);
         byte[] qrCodeBytes = Files.readAllBytes(qrCodeFile.toPath());
         byte[] qrCodeBase64Bytes = Base64Utils.encode(qrCodeBytes);
         String qrCodeBase64String = new String(qrCodeBase64Bytes);
@@ -81,7 +75,7 @@ public class DGCTestJSONGenerator {
         dgcTestJSON.setPrefix(qrCodeContent);
 
         // Getting the content of the QR code without prefix.
-        String qrCodeContentWithoutPrefix = qrCodeContent.replaceFirst("HC1:", "");
+        String qrCodeContentWithoutPrefix = qrCodeContent.replaceFirst(PREFIX, "");
         dgcTestJSON.setBase45(qrCodeContentWithoutPrefix);
 
         // Getting the content of the QR code base45 decoded.
@@ -98,33 +92,56 @@ public class DGCTestJSONGenerator {
         CBORObject coseCBORObject = CBORObject.DecodeFromBytes(qrCodeContentDecompressedBytes);
         byte[] cosePayloadBytes = coseCBORObject.get(2).GetByteString();
         CBORObject cosePayloadCBORObject = CBORObject.DecodeFromBytes(cosePayloadBytes);
-        byte[] hcertCBORBytes = cosePayloadCBORObject.get(-260).get(1).EncodeToBytes();
-        String hcertHexString = Hex.encodeHexString(hcertCBORBytes);
-        dgcTestJSON.setCbor(hcertHexString);
+        byte[] hCertCBORBytes = cosePayloadCBORObject.get(-260).get(1).EncodeToBytes();
+        String hCertHexString = Hex.encodeHexString(hCertCBORBytes);
+        dgcTestJSON.setCbor(hCertHexString);
 
         // Getting the certificate JSON as string.
-        CBORObject hcertCBORObject = CBORObject.DecodeFromBytes(hcertCBORBytes);
-        String hcert = hcertCBORObject.ToJSONString();
+        CBORObject hCertCBORObject = CBORObject.DecodeFromBytes(hCertCBORBytes);
+        String hCert = hCertCBORObject.ToJSONString();
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
-        Object hcertObject = null;
-        if (TYPE == CertificateType.Vaccination) {
-            hcertObject = objectMapper.readValue(hcert, VaccinationCertificateQrCode.class);
-        } else if (TYPE == CertificateType.Test) {
-            hcertObject = objectMapper.readValue(hcert, TestCertificateQrCode.class);
-        } else if (TYPE == CertificateType.Recovery) {
-            hcertObject = objectMapper.readValue(hcert, RecoveryCertificateQrCode.class);
+        Object hCertObject = null;
+        String certificateType = null;
+        if (hCert.contains(IDENTIFIER_VACCINATION)) {
+            hCertObject = objectMapper.readValue(hCert, VaccinationCertificateQrCode.class);
+            certificateType = "Vaccination";
+        } else if (hCert.contains(IDENTIFIER_TEST)) {
+            hCertObject = objectMapper.readValue(hCert, TestCertificateQrCode.class);
+            certificateType = "Test";
+        } else if (hCert.contains(IDENTIFIER_RECOVERY)) {
+            hCertObject = objectMapper.readValue(hCert, RecoveryCertificateQrCode.class);
+            certificateType = "Recovery";
         }
-        dgcTestJSON.setJson(hcertObject);
+        dgcTestJSON.setJson(hCertObject);
+
+        // Setting the test context.
+        DGCTestJSONTestContext dgcTestJSONTestContext = new DGCTestJSONTestContext();
+        dgcTestJSONTestContext.setVersion(VERSION);
+        dgcTestJSONTestContext.setJsonSchema(JSON_SCHEMA);
+        dgcTestJSONTestContext.setCertificate(CERTIFICATE);
+        dgcTestJSONTestContext.setValidationClock(OffsetDateTime.now(Clock.system(ZoneId.of(ZONE_ID))).withNano(0).toString());
+        dgcTestJSONTestContext.setDescription(DESCRIPTION + certificateType);
+        dgcTestJSON.setTestContext(dgcTestJSONTestContext);
+
+        // Setting the expected results.
+        DGCTestJSONExpectedResults dgcTestJSONExpectedResults = new DGCTestJSONExpectedResults();
+        dgcTestJSONExpectedResults.setExpectedValidObject(true);
+        dgcTestJSONExpectedResults.setExpectedSchemaValidation(true);
+        dgcTestJSONExpectedResults.setExpectedEncode(true);
+        dgcTestJSONExpectedResults.setExpectedDecode(true);
+        dgcTestJSONExpectedResults.setExpectedVerify(true);
+        dgcTestJSONExpectedResults.setExpectedCompression(true);
+        dgcTestJSONExpectedResults.setExpectedKeyUsage(true);
+        dgcTestJSONExpectedResults.setExpectedUnPrefix(true);
+        dgcTestJSONExpectedResults.setExpectedValidJSON(true);
+        dgcTestJSONExpectedResults.setExpectedB45Decode(true);
+        dgcTestJSONExpectedResults.setExpectedPictureDecode(true);
+        dgcTestJSONExpectedResults.setExpectedExpirationCheck(true);
+        dgcTestJSON.setExpectedResults(dgcTestJSONExpectedResults);
 
         // Creating the JSON file.
         objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
-        objectMapper.writeValue(new File("target/dgc-testdata/1.json"), dgcTestJSON);
-    }
-
-    private enum CertificateType {
-        Vaccination,
-        Test,
-        Recovery
+        objectMapper.writeValue(new File(PATH + qrCodeFilename.replace(PNG, JSON)), dgcTestJSON);
     }
 }
