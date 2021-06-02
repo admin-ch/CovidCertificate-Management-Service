0aa6f3dc6e439a4e8db0688302a7d1bc85c2ff8f
ErMejo74 <ErMejo74@users.noreply.github.com>
2021-06-02 21:34:05 +0200

    Uses the correct type for timestamps.
    See https://wiki.postgresql.org/wiki/Don%27t_Do_This#Don.27t_use_timestamp_.28without_time_zone.29

--- src/main/resources/db/migration/common/V1_0_0__create-schema.sql
+++ src/main/resources/db/migration/common/V1_0_0__create-schema.sql
@@ -2,5 +2,5 @@ create table revocation
 (
     id uuid not null primary key,
     uvci varchar(39) not null unique,
-	creation_date_time timestamp not null default now()
+	creation_date_time timestamptz not null default now()
 );
