3c74bed8db117eb49099c6aad918b76b269f83e8
Fabien Cerf <fabien.cerf@bit.admin.ch>
2021-06-10 14:00:03 +0200

    VACCINECER-853: update libs

--- src/test/java/ch/admin/bag/covidcertificate/web/controller/CovidCertificateGenerationControllerSecurityTest.java
+++ src/test/java/ch/admin/bag/covidcertificate/web/controller/CovidCertificateGenerationControllerSecurityTest.java
@@ -83,6 +83,113 @@ class CovidCertificateGenerationControllerSecurityTest {
                 .withStatus(HttpStatus.OK.value())
                 .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
                 .withBody(KEY_PAIR_TEST_UTIL.getJwks())));
+        wireMockServer.stubFor(get(urlPathEqualTo("/.well-known/openid-configuration")).willReturn(aResponse()
+                .withStatus(HttpStatus.OK.value())
+                .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                .withBody("{\n" +
+                        "  \"issuer\": \"http://localhost:8182\",\n" +
+                        "  \"authorization_endpoint\": \"http://localhost:8182/oauth/authorize\",\n" +
+                        "  \"token_endpoint\": \"http://localhost:8182/oauth/token\",\n" +
+                        "  \"userinfo_endpoint\": \"http://localhost:8182/userinfo\",\n" +
+                        "  \"end_session_endpoint\": \"http://localhost:8182/logout\",\n" +
+                        "  \"jwks_uri\": \"http://localhost:8182/.well-known/jwks.json\",\n" +
+                        "  \"grant_types_supported\": [\n" +
+                        "    \"authorization_code\",\n" +
+                        "    \"refresh_token\",\n" +
+                        "    \"client_credentials\"\n" +
+                        "  ],\n" +
+                        "  \"response_types_supported\": [\n" +
+                        "    \"code\",\n" +
+                        "    \"none\",\n" +
+                        "    \"id_token\",\n" +
+                        "    \"token\",\n" +
+                        "    \"id_token token\",\n" +
+                        "    \"code id_token\",\n" +
+                        "    \"code token\",\n" +
+                        "    \"code id_token token\"\n" +
+                        "  ],\n" +
+                        "  \"subject_types_supported\": [\n" +
+                        "    \"public\",\n" +
+                        "    \"pairwise\"\n" +
+                        "  ],\n" +
+                        "  \"id_token_signing_alg_values_supported\": [\n" +
+                        "    \"ES384\",\n" +
+                        "    \"RS384\",\n" +
+                        "    \"HS256\",\n" +
+                        "    \"HS512\",\n" +
+                        "    \"ES256\",\n" +
+                        "    \"RS256\",\n" +
+                        "    \"HS384\",\n" +
+                        "    \"ES512\",\n" +
+                        "    \"RS512\"\n" +
+                        "  ],\n" +
+                        "  \"userinfo_signing_alg_values_supported\": [\n" +
+                        "    \"ES384\",\n" +
+                        "    \"RS384\",\n" +
+                        "    \"HS256\",\n" +
+                        "    \"HS512\",\n" +
+                        "    \"ES256\",\n" +
+                        "    \"RS256\",\n" +
+                        "    \"HS384\",\n" +
+                        "    \"ES512\",\n" +
+                        "    \"RS512\",\n" +
+                        "    \"none\"\n" +
+                        "  ],\n" +
+                        "  \"request_object_signing_alg_values_supported\": [\n" +
+                        "    \"ES384\",\n" +
+                        "    \"RS384\",\n" +
+                        "    \"ES256\",\n" +
+                        "    \"RS256\",\n" +
+                        "    \"ES512\",\n" +
+                        "    \"RS512\",\n" +
+                        "    \"none\"\n" +
+                        "  ],\n" +
+                        "  \"response_modes_supported\": [\n" +
+                        "    \"query\",\n" +
+                        "    \"fragment\",\n" +
+                        "    \"form_post\"\n" +
+                        "  ],\n" +
+                        "  \"token_endpoint_auth_methods_supported\": [\n" +
+                        "    \"private_key_jwt\",\n" +
+                        "    \"client_secret_basic\",\n" +
+                        "    \"client_secret_post\",\n" +
+                        "    \"client_secret_jwt\"\n" +
+                        "  ],\n" +
+                        "  \"token_endpoint_auth_signing_alg_values_supported\": [\n" +
+                        "    \"RS256\"\n" +
+                        "  ],\n" +
+                        "  \"claims_supported\": [\n" +
+                        "    \"sub\",\n" +
+                        "    \"iss\",\n" +
+                        "    \"auth_time\",\n" +
+                        "    \"name\",\n" +
+                        "    \"given_name\",\n" +
+                        "    \"family_name\",\n" +
+                        "    \"preferred_username\",\n" +
+                        "    \"ext_id\",\n" +
+                        "    \"login_level\",\n" +
+                        "    \"email\"\n" +
+                        "  ],\n" +
+                        "  \"claim_types_supported\": [\n" +
+                        "    \"normal\"\n" +
+                        "  ],\n" +
+                        "  \"claims_parameter_supported\": false,\n" +
+                        "  \"scopes_supported\": [\n" +
+                        "    \"openid\",\n" +
+                        "    \"offline_access\",\n" +
+                        "    \"profile\",\n" +
+                        "    \"roles\",\n" +
+                        "    \"web-origins\"\n" +
+                        "  ],\n" +
+                        "  \"request_parameter_supported\": true,\n" +
+                        "  \"request_uri_parameter_supported\": true,\n" +
+                        "  \"code_challenge_methods_supported\": [\n" +
+                        "    \"plain\",\n" +
+                        "    \"S256\"\n" +
+                        "  ],\n" +
+                        "  \"tls_client_certificate_bound_access_tokens\": true,\n" +
+                        "  \"introspection_endpoint\": \"http://localhost:8182/protocol/openid-connect/token/introspect\"\n" +
+                        "}")));
     }
 
     @BeforeEach
@@ -97,7 +204,7 @@ class CovidCertificateGenerationControllerSecurityTest {
         wireMockServer.stop();
     }
 
-    
+
     @Nested
     class CreateVaccinationCertificate {
         private static final String URL = BASE_URL+"vaccination";
@@ -119,7 +226,7 @@ class CovidCertificateGenerationControllerSecurityTest {
             callCreateVaccinationCertificateWithToken(EXPIRED_IN_PAST, VALID_USER_ROLE, HttpStatus.UNAUTHORIZED);
             Mockito.verify(covidCertificateGenerationService, times(0)).generateCovidCertificate(any(VaccinationCertificateCreateDto.class));
         }
-        
+
         private void callCreateVaccinationCertificateWithToken(LocalDateTime tokenExpiration, String userRole, HttpStatus status) throws Exception {
             var createDto = fixture.create(VaccinationCertificateCreateDto.class);
             callCreateCertificateWithToken(URL, mapper.writeValueAsString(createDto), tokenExpiration, userRole, status);
