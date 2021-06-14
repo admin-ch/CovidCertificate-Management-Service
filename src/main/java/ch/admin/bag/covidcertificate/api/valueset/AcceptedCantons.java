11d537583490eed57f0891a526fc9a2f4f3e77ba
Fabien Cerf <fabien.cerf@bit.admin.ch>
2021-06-14 10:38:58 +0200

    VACCINECER-955: validate canton

--- src/main/java/ch/admin/bag/covidcertificate/api/valueset/AcceptedCantons.java
+++ src/main/java/ch/admin/bag/covidcertificate/api/valueset/AcceptedCantons.java
@@ -4,12 +4,11 @@ import java.util.Arrays;
 
 public class AcceptedCantons {
 
-    public enum Values {
+    private enum cantons {
         AG, AI, AR, BE, BL, BS, FR, GE, GL, GR, JU, LU, NE, NW, OW, SG, SH, SO, SZ, TG, TI, UR, VD, VS, ZG, ZH
     }
 
     public static boolean isAccepted(String canton) {
-        return Arrays.stream(Values.values())
-                .anyMatch((currentCanton) -> currentCanton.name().equalsIgnoreCase(canton));
+        return Arrays.stream(cantons.values()).anyMatch(currentCanton -> currentCanton.name().equalsIgnoreCase(canton));
     }
 }
