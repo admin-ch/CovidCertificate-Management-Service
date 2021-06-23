529eef62e2b2b5cce483dca89ba4239267c07d48
Mathias Wegmüller <matthias.wegmueller@bit.admin.ch>
2021-06-23 09:22:34 +0200

    VACCINECER-952 Changed error codes for standardised names for new JSON schema 1.3.0.

--- src/main/java/ch/admin/bag/covidcertificate/api/Constants.java
+++ src/main/java/ch/admin/bag/covidcertificate/api/Constants.java
@@ -65,8 +65,8 @@ public class Constants {
     public static final CreateCertificateError INVALID_ADDRESS = new CreateCertificateError(474, "Paper-based delivery requires a valid address.", HttpStatus.BAD_REQUEST);
     public static final CreateCertificateError DUPLICATE_DELIVERY_METHOD = new CreateCertificateError(475, "Delivery method can either be InApp or print, but not both.", HttpStatus.BAD_REQUEST);
     public static final CreateCertificateError INVALID_IN_APP_CODE = new CreateCertificateError(476, "InAppDelivery-Code is invalid.", HttpStatus.NOT_FOUND);
-    public static final CreateCertificateError INVALID_STANDARDISED_GIVEN_NAME = new CreateCertificateError(475, "Invalid given name! The standardised given name exceeds 80 chars", HttpStatus.BAD_REQUEST);
-    public static final CreateCertificateError INVALID_STANDARDISED_FAMILY_NAME = new CreateCertificateError(476, "Invalid family name! The standardised family name exceeds 80 chars", HttpStatus.BAD_REQUEST);
+    public static final CreateCertificateError INVALID_STANDARDISED_GIVEN_NAME = new CreateCertificateError(477, "Invalid given name! The standardised given name exceeds 80 chars", HttpStatus.BAD_REQUEST);
+    public static final CreateCertificateError INVALID_STANDARDISED_FAMILY_NAME = new CreateCertificateError(478, "Invalid family name! The standardised family name exceeds 80 chars", HttpStatus.BAD_REQUEST);
 
     public static final RevocationError DUPLICATE_UVCI = new RevocationError(480, "Duplicate UVCI.", HttpStatus.CONFLICT);
 
