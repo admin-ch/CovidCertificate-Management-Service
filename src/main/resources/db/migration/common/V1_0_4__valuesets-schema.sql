d89890074e11df8272b4cad0014ce7f0bea5b597
Mathias Wegmüller <matthias.wegmueller@bit.admin.ch>
2021-08-16 16:44:52 +0200

    VACCINECER-1183 Change database schema for vaccination value sets from EU.

--- src/main/resources/db/migration/common/V1_0_4__valuesets-schema.sql
+++ src/main/resources/db/migration/common/V1_0_4__valuesets-schema.sql
@@ -1,3 +1,12 @@
+create table sct_vaccines_covid_19
+(
+    id uuid not null primary key,
+    code varchar(50) not null unique,
+    display varchar(100) not null,
+    active boolean not null,
+    modified_at timestamp not null default now()
+);
+
 create table vaccines_covid_19_auth_holders
 (
     id uuid not null primary key,
@@ -12,7 +21,8 @@ create table vaccines_covid_19_names
     id uuid not null primary key,
     code varchar(50) not null unique,
     display varchar(100) not null,
-    auth_holders varchar(50) not null REFERENCES vaccines_covid_19_auth_holders (code),
+    prophylaxis uuid not null REFERENCES sct_vaccines_covid_19 (id),
+    auth_holder uuid not null REFERENCES vaccines_covid_19_auth_holders (id),
     active boolean not null,
     ch_issuable boolean not null default false,
     modified_at timestamp not null default now()
@@ -21,7 +31,7 @@ create table vaccines_covid_19_names
 create table covid_19_lab_test_manufacturer_and_name
 (
     id uuid not null primary key,
-    code varchar(10) not null unique,
+    code varchar(50) not null unique,
     display varchar(200) not null,
     active boolean not null,
     ch_issuable boolean not null default false,
