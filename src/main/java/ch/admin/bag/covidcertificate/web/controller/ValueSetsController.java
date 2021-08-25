d079ceab2ff24b7524660780a08340193cd01a41
Mathias Wegmüller <matthias.wegmueller@bit.admin.ch>
2021-08-25 19:57:21 +0200

    VACCINECER-1263 Add mock service methods for value sets from EU.

--- src/main/java/ch/admin/bag/covidcertificate/web/controller/ValueSetsController.java
+++ src/main/java/ch/admin/bag/covidcertificate/web/controller/ValueSetsController.java
@@ -37,10 +37,7 @@ public class ValueSetsController {
         log.info("Call of getRapidTests for value sets");
         securityHelper.authorizeUser(request);
 
-        var rapidTests = new ArrayList<RapidTestDto>();
-        rapidTests.add(new RapidTestDto("1341", "Qingdao Hightop Biotech Co., Ltd, SARS-CoV-2 Antigen Rapid Test (Immunochromatography)", true));
-        rapidTests.add(new RapidTestDto("1065", "Becton Dickinson, BD Veritor? System for Rapid Detection of SARS CoV 2", true));
-        return rapidTests;
+        return valueSetsService.getRapidTests();
     }
 
     @PostMapping("/issuable-rapid-tests")
@@ -48,10 +45,7 @@ public class ValueSetsController {
         log.info("Call of getIssuableRapidTests for value sets");
         securityHelper.authorizeUser(request);
 
-        var issuableRapidTests = new ArrayList<IssuableRapidTestDto>();
-        issuableRapidTests.add(new IssuableRapidTestDto("1341", "Qingdao Hightop Biotech Co., Ltd, SARS-CoV-2 Antigen Rapid Test (Immunochromatography)"));
-        issuableRapidTests.add(new IssuableRapidTestDto("1065", "Becton Dickinson, BD Veritor? System for Rapid Detection of SARS CoV 2"));
-        return issuableRapidTests;
+        return valueSetsService.getIssuableRapidTests();
     }
 
     @PostMapping("/vaccines")
@@ -59,10 +53,7 @@ public class ValueSetsController {
         log.info("Call of getVaccines for value sets");
         securityHelper.authorizeUser(request);
 
-        var vaccines = new ArrayList<VaccineDto>();
-        vaccines.add(new VaccineDto("EU/1/20/1528", "Comirnaty", "1119349007", "SARS-CoV-2 mRNA vaccine", "ORG-100030215", "Biontech Manufacturing GmbH", true));
-        vaccines.add(new VaccineDto("EU/1/20/1507", "COVID-19 Vaccine Moderna", "1119349007", "SARS-CoV-2 mRNA vaccine", "ORG-100031184", "Moderna Biotech Spain S.L.", true));
-        return vaccines;
+        return valueSetsService.getVaccines();
     }
 
     @PostMapping("/issuable-vaccines")
@@ -70,9 +61,6 @@ public class ValueSetsController {
         log.info("Call of getIssuableVaccines for value sets");
         securityHelper.authorizeUser(request);
 
-        var issuableVaccines = new ArrayList<IssuableVaccineDto>();
-        issuableVaccines.add(new IssuableVaccineDto("EU/1/20/1528", "Comirnaty", "1119349007", "SARS-CoV-2 mRNA vaccine", "ORG-100030215", "Biontech Manufacturing GmbH"));
-        issuableVaccines.add(new IssuableVaccineDto("EU/1/20/1507", "COVID-19 Vaccine Moderna", "1119349007", "SARS-CoV-2 mRNA vaccine", "ORG-100031184", "Moderna Biotech Spain S.L."));
-        return issuableVaccines;
+        return valueSetsService.getIssuableVaccines();
     }
 }
