d4a218c7791865acbaf77d93e65a4994a1a87210
Fabian Zbinden <fabian.zbinden@bit.admin.ch>
2021-10-25 14:41:30 +0200

    VACCINECER-974 generation API now validates against most common special symbols for names (blacklist approach)

--- src/main/java/ch/admin/bag/covidcertificate/api/request/CovidCertificatePersonNameDto.java
+++ src/main/java/ch/admin/bag/covidcertificate/api/request/CovidCertificatePersonNameDto.java
@@ -4,6 +4,9 @@ import ch.admin.bag.covidcertificate.api.exception.CreateCertificateException;
 import lombok.*;
 import org.springframework.util.StringUtils;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import static ch.admin.bag.covidcertificate.api.Constants.*;
 
 @Getter
@@ -17,10 +20,23 @@ public class CovidCertificatePersonNameDto {
     private String givenName;
 
     public void validate() {
+        Pattern p = Pattern.compile("[!@#\\r\\n$%¶*\\\\()_:/+=|<>?{}\\[\\]~-]");
+
+
         if (!StringUtils.hasText(givenName) || givenName.length() > MAX_STRING_LENGTH) {
             throw new CreateCertificateException(INVALID_GIVEN_NAME);
         }
         if (!StringUtils.hasText(familyName) || familyName.length() > MAX_STRING_LENGTH) {
+            throw new CreateCertificateException(INVALID_FAMILY_NAME);
+        }
+
+        Matcher givenNameMatcher = p.matcher(givenName);
+        Matcher familyNameMatcher = p.matcher(familyName);
+
+        if(givenNameMatcher.find()) {
+            throw new CreateCertificateException(INVALID_GIVEN_NAME);
+        }
+        if(familyNameMatcher.find()) {
             throw new CreateCertificateException(INVALID_FAMILY_NAME);
         }
     }
