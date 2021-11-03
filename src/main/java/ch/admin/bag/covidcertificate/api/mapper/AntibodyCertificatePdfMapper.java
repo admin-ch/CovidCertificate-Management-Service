9ed4e8039207ec1ed180a2554d50ff2bda6c41bb
mofobo <mohammed.fonsecaboukhalat@bit.admin.ch>
2021-11-03 15:41:23 +0100

    add pdf and qr-code mappers

---
+++ src/main/java/ch/admin/bag/covidcertificate/api/mapper/AntibodyCertificatePdfMapper.java
@@ -0,0 +1,36 @@
+package ch.admin.bag.covidcertificate.api.mapper;
+
+import ch.admin.bag.covidcertificate.api.request.AntibodyCertificateCreateDto;
+import ch.admin.bag.covidcertificate.service.domain.CovidCertificateDiseaseOrAgentTargeted;
+import ch.admin.bag.covidcertificate.service.domain.AntibodyCertificatePdf;
+import ch.admin.bag.covidcertificate.service.domain.AntibodyCertificateQrCode;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import static ch.admin.bag.covidcertificate.api.Constants.ISSUER;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class AntibodyCertificatePdfMapper {
+
+    public static AntibodyCertificatePdf toAntibodyCertificatePdf(
+            AntibodyCertificateCreateDto antibodyCertificateCreateDto,
+            AntibodyCertificateQrCode qrCodeData,
+            String countryOfTest,
+            String countryOfTestEn
+    ) {
+        CovidCertificateDiseaseOrAgentTargeted diseaseOrAgentTargeted = CovidCertificateDiseaseOrAgentTargeted.getStandardInstance();
+        return new AntibodyCertificatePdf(
+                antibodyCertificateCreateDto.getPersonData().getName().getFamilyName(),
+                antibodyCertificateCreateDto.getPersonData().getName().getGivenName(),
+                antibodyCertificateCreateDto.getPersonData().getDateOfBirth(),
+                antibodyCertificateCreateDto.getLanguage(),
+                diseaseOrAgentTargeted.getCode(),
+                diseaseOrAgentTargeted.getSystem(),
+                antibodyCertificateCreateDto.getAntibodyInfo().get(0).getSampleDate(),
+                antibodyCertificateCreateDto.getAntibodyInfo().get(0).getTestingCentreOrFacility(),
+                countryOfTest,
+                countryOfTestEn,
+                ISSUER,
+                qrCodeData.getAntibodyInfo().get(0).getIdentifier());
+    }
+}
