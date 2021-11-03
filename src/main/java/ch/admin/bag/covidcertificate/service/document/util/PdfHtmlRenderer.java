47482d7b93acc572ec696c911c16b3be26147971
mofobo <mohammed.fonsecaboukhalat@bit.admin.ch>
2021-11-03 22:32:51 +0100

    pass "showMark" as parameter

--- src/main/java/ch/admin/bag/covidcertificate/service/document/util/PdfHtmlRenderer.java
+++ src/main/java/ch/admin/bag/covidcertificate/service/document/util/PdfHtmlRenderer.java
@@ -3,7 +3,6 @@ package ch.admin.bag.covidcertificate.service.document.util;
 import ch.admin.bag.covidcertificate.service.document.CustomMessageResolver;
 import ch.admin.bag.covidcertificate.service.domain.AbstractCertificatePdf;
 import ch.admin.bag.covidcertificate.util.DateHelper;
-import org.springframework.beans.factory.annotation.Value;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.templatemode.TemplateMode;
@@ -18,9 +17,6 @@ import static ch.admin.bag.covidcertificate.api.Constants.LOCAL_DATE_FORMAT;
 public class PdfHtmlRenderer {
 
     private final TemplateEngine templateEngine;
-
-    @Value("${cc-management-service.pdf.show-watermark}")
-    private boolean showWatermark;
 
     public PdfHtmlRenderer() {
         this.templateEngine = this.getTemplateEngine();
@@ -38,17 +34,17 @@ public class PdfHtmlRenderer {
         return templateEngine;
     }
 
-    public String render(AbstractCertificatePdf data, String barcodeImage, LocalDateTime issuedAt) {
-        var context = this.getContext(data, barcodeImage, issuedAt);
+    public String render(AbstractCertificatePdf data, String barcodeImage, LocalDateTime issuedAt, boolean showWatermark) {
+        var context = this.getContext(data, barcodeImage, issuedAt, showWatermark);
         return templateEngine.process("pdf", context);
     }
 
-    private Context getContext(AbstractCertificatePdf data, String barcodeImage, LocalDateTime issuedAt) {
+    private Context getContext(AbstractCertificatePdf data, String barcodeImage, LocalDateTime issuedAt, boolean showWatermark) {
         var context = new Context();
         context.setLocale(this.getLocale(data.getLanguage()));
         context.setVariable("data", data);
         context.setVariable("isEvidence", data.isEvidence());
-        context.setVariable("showWatermark", this.showWatermark);
+        context.setVariable("showWatermark", showWatermark);
         context.setVariable("qrCode", barcodeImage);
         context.setVariable("dateFormatter", LOCAL_DATE_FORMAT);
         context.setVariable("creationDate", issuedAt.format(LOCAL_DATE_FORMAT));
