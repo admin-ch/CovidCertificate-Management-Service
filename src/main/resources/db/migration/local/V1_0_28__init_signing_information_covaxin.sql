5d74d4c2fc7ee5440af404f3db154a70d0cfcccd
Harald Lösing <harald.loesing@bit.admin.ch>
2021-11-11 09:14:58 +0100

    insert and activate covaxin, corrections for local development.

---
+++ src/main/resources/db/migration/local/V1_0_28__init_signing_information_covaxin.sql
@@ -0,0 +1,2 @@
+INSERT INTO signing_information (id, certificate_type, code, alias, key_identifier)
+VALUES ('20b46fb3-0251-4763-9bea-6a141ee2d17c', 'vaccination', 'Covaxin', 'mock', 'YXpNbFM4b01sUXM9');
