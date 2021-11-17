46d01359676c2d444aaaecd1e0237431d995222d
Harald Lösing <harald.loesing@bit.admin.ch>
2021-11-17 07:27:30 +0100

    fixed instance usage of PdfHtmlRenderer.

--- src/main/java/ch/admin/bag/covidcertificate/service/document/PdfCertificateGenerationService.java
+++ src/main/java/ch/admin/bag/covidcertificate/service/document/PdfCertificateGenerationService.java
@@ -10,7 +10,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
@@ -24,18 +23,12 @@ import java.util.Objects;
 public class PdfCertificateGenerationService {
 
     private final PdfRendererBuilder pdfBuilder;
-    private PdfHtmlRenderer pdfHtmlRenderer;
 
     @Value("#{new Boolean('${cc-management-service.pdf.show-watermark}')}")
     private boolean showWatermark;
 
     public PdfCertificateGenerationService() {
         this.pdfBuilder = getPdfBuilder();
-    }
-
-    @PostConstruct
-    public void postConstruct(){
-        this.pdfHtmlRenderer = new PdfHtmlRenderer(this.showWatermark);
     }
 
     private PdfRendererBuilder getPdfBuilder() {
@@ -53,7 +46,8 @@ public class PdfCertificateGenerationService {
     public byte[] generateCovidCertificate(AbstractCertificatePdf data, String barcodePayload, LocalDateTime issuedAt) {
         try {
             var templatePath = this.getClass().getClassLoader().getResource("templates/pdf.html");
-            var content = this.pdfHtmlRenderer.render(data, this.getBarcodeImage(barcodePayload), issuedAt);
+            PdfHtmlRenderer pdfHtmlRenderer = new PdfHtmlRenderer(this.showWatermark);
+            var content = pdfHtmlRenderer.render(data, this.getBarcodeImage(barcodePayload), issuedAt);
 
             var os = new ByteArrayOutputStream();
             pdfBuilder.toStream(os);
--- src/main/java/ch/admin/bag/covidcertificate/service/document/util/PdfHtmlRenderer.java
+++ src/main/java/ch/admin/bag/covidcertificate/service/document/util/PdfHtmlRenderer.java
@@ -79,5 +79,4 @@ public class PdfHtmlRenderer {
                 return Locale.GERMAN;
         }
     }
-
 }
