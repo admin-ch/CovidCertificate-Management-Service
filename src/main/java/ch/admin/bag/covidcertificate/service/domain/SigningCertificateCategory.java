1c24d4e80f446c8a666c45cdb0c1a88dfce414e6
mofobo <mohammed.fonsecaboukhalat@bit.admin.ch>
2021-11-22 19:34:52 +0100

    add signing certificate category

--- src/main/java/ch/admin/bag/covidcertificate/service/domain/SigningCertificateCategory.java
+++ src/main/java/ch/admin/bag/covidcertificate/service/domain/SigningCertificateCategory.java
@@ -2,6 +2,7 @@ package ch.admin.bag.covidcertificate.service.domain;
 
 public enum SigningCertificateCategory {
     VACCINATION("vaccination"),
+    VACCINATION_TOURIST_CH("vaccination_tourist_ch"),
     RECOVERY_CH("recovery_ch"),
     RECOVERY_NON_CH("recovery_non_ch"),
     TEST("test"),
