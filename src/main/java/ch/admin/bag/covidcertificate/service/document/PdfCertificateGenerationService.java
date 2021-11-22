c8f9c9fa30bde4390bb269a0b5134792e4f04a7d
mofobo <mohammed.fonsecaboukhalat@bit.admin.ch>
2021-11-22 16:25:23 +0100

    Revert "!!! optimize resources (critical section identified and handled correctly) !!!"
    This reverts commit b1e911d91e08713dc23b23b52f7a31099eaea720.

--- src/main/java/ch/admin/bag/covidcertificate/service/document/PdfCertificateGenerationService.java
+++ src/main/java/ch/admin/bag/covidcertificate/service/document/PdfCertificateGenerationService.java
@@ -10,11 +10,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
-import java.net.URL;
 import java.time.LocalDateTime;
 import java.util.EnumMap;
 import java.util.Map;
@@ -24,43 +22,31 @@ import java.util.Objects;
 @Slf4j
 public class PdfCertificateGenerationService {
 
-    private File fontArial500;
-    private File fontArialBold600;
-    private File fontArialItalic500;
-    private URL templatePath;
-    private PdfHtmlRenderer pdfHtmlRenderer;
-
     @Value("#{new Boolean('${cc-management-service.pdf.show-watermark}')}")
     private boolean showWatermark;
 
-    @PostConstruct
-    public void postConstruct() {
-        ClassLoader classLoader = this.getClass().getClassLoader();
-        this.templatePath = this.getClass().getClassLoader().getResource("templates/pdf.html");
-        this.fontArial500 = new File(Objects.requireNonNull(classLoader.getResource("templates/fonts/arial.ttf")).getPath());
-        this.fontArialBold600 = new File(Objects.requireNonNull(classLoader.getResource("templates/fonts/arialbd.ttf")).getPath());
-        this.fontArialItalic500 = new File(Objects.requireNonNull(classLoader.getResource("templates/fonts/ariali.ttf")).getPath());
-        this.pdfHtmlRenderer = new PdfHtmlRenderer(this.showWatermark);
-    }
-
     private PdfRendererBuilder createPdfBuilder() {
+        var classLoader = this.getClass().getClassLoader();
         var builder = new PdfRendererBuilder();
         builder.useFastMode();
         builder.usePdfUaAccessbility(true);
-        builder.useFont(fontArial500, "Arial", 500, BaseRendererBuilder.FontStyle.NORMAL, false);
-        builder.useFont(fontArialBold600, "Arial", 600, BaseRendererBuilder.FontStyle.NORMAL, true);
-        builder.useFont(fontArialItalic500, "Arial", 500, BaseRendererBuilder.FontStyle.ITALIC, true);
+        builder.useFont(new File(Objects.requireNonNull(classLoader.getResource("templates/fonts/arial.ttf")).getPath()), "Arial", 500, BaseRendererBuilder.FontStyle.NORMAL, false);
+        builder.useFont(new File(Objects.requireNonNull(classLoader.getResource("templates/fonts/arialbd.ttf")).getPath()), "Arial", 600, BaseRendererBuilder.FontStyle.NORMAL, true);
+        builder.useFont(new File(Objects.requireNonNull(classLoader.getResource("templates/fonts/ariali.ttf")).getPath()), "Arial", 500, BaseRendererBuilder.FontStyle.ITALIC, true);
         builder.usePdfAConformance(PdfRendererBuilder.PdfAConformance.PDFA_3_U);
         return builder;
     }
 
     public byte[] generateCovidCertificate(AbstractCertificatePdf data, String barcodePayload, LocalDateTime issuedAt) {
         try {
+            var templatePath = this.getClass().getClassLoader().getResource("templates/pdf.html");
+            PdfHtmlRenderer pdfHtmlRenderer = new PdfHtmlRenderer(this.showWatermark);
             var content = pdfHtmlRenderer.render(data, this.getBarcodeImage(barcodePayload), issuedAt);
+
             var os = new ByteArrayOutputStream();
             PdfRendererBuilder pdfBuilder = createPdfBuilder();
             pdfBuilder.toStream(os);
-            pdfBuilder.withHtmlContent(content, Objects.requireNonNull(this.templatePath).toString());
+            pdfBuilder.withHtmlContent(content, Objects.requireNonNull(templatePath).toString());
             pdfBuilder.run();
 
             return os.toByteArray();
