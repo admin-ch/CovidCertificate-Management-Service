c5ce88e5706181b31c51fdfd5c9a7d713b54ff73
mofobo <mohammed.fonsecaboukhalat@bit.admin.ch>
2021-11-22 13:25:21 +0100

    add static values for the address limits and refactor tests (improve readability)

--- src/test/java/ch/admin/bag/covidcertificate/api/request/CovidCertificateAddressDtoTest.java
+++ src/test/java/ch/admin/bag/covidcertificate/api/request/CovidCertificateAddressDtoTest.java
@@ -1,117 +1,159 @@
 package ch.admin.bag.covidcertificate.api.request;
 
 import ch.admin.bag.covidcertificate.api.exception.CreateCertificateException;
-import ch.admin.bag.covidcertificate.api.valueset.AllowedSenders;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.http.HttpStatus;
 
 import static ch.admin.bag.covidcertificate.api.Constants.INVALID_ADDRESS;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@Tag("CovidCertificateAddressDtoTest")
+@DisplayName("Tests for the CovidCertificateAddressDto")
 class CovidCertificateAddressDtoTest {
 
-    @Test
-    void createsAddressDto__ifAllValid() {
-        var addressDto = new CovidCertificateAddressDto("streetAndNr", 1000, "city", "BE");
-        assertDoesNotThrow(addressDto::validate);
-    }
+    private final int MAX_STREETANDNR_CHARS_LENGTH = 128;
+    private final int MAX_CITY_CHARS_LENGTH = 128;
+    private final int MIN_ZIPCODE_VALUE = 1000;
+    private final int MAX_ZIPCODE_VALUE = 9999;
 
-    @Test
-    void throwsCertificateCreateException__onEmptyStreet() {
-        var addressDto = new CovidCertificateAddressDto(null, 1000, "city", "BE");
-        CreateCertificateException exception = assertThrows(CreateCertificateException.class, addressDto::validate);
-        assertEquals(INVALID_ADDRESS, exception.getError());
-
-        addressDto = new CovidCertificateAddressDto("", 1000, "city", "BE");
-        exception = assertThrows(CreateCertificateException.class, addressDto::validate);
-        assertEquals(INVALID_ADDRESS, exception.getError());
-
-        addressDto = new CovidCertificateAddressDto("   ", 1000, "city", "BE");
-        exception = assertThrows(CreateCertificateException.class, addressDto::validate);
+    private void assertInvalidAddressError(CovidCertificateAddressDto covidCertificateAddressDto) {
+        CreateCertificateException exception = assertThrows(CreateCertificateException.class, covidCertificateAddressDto::validate);
+        assertEquals(HttpStatus.BAD_REQUEST, exception.getError().getHttpStatus());
+        assertEquals(474, exception.getError().getErrorCode());
+        assertEquals("Paper-based delivery requires a valid address.", exception.getError().getErrorMessage());
         assertEquals(INVALID_ADDRESS, exception.getError());
     }
 
     @Test
-    void throwsCertificateCreateException__ifStreetTooLong() {
-        String street = RandomStringUtils.random(129, true, true);
-        var addressDto = new CovidCertificateAddressDto(street, 1000, "city", "BE");
-        CreateCertificateException exception = assertThrows(CreateCertificateException.class, addressDto::validate);
-        assertEquals(INVALID_ADDRESS, exception.getError());
+    @DisplayName("Given all the parameters for CovidCertificateAddressDto are valid, when validated, it should not throw an exception.")
+    void CovidCertificateAddressDtoTest1() {
+        var covidCertificateAddressDto = new CovidCertificateAddressDto("streetAndNr", MIN_ZIPCODE_VALUE, "city", "BE");
+        assertDoesNotThrow(covidCertificateAddressDto::validate);
     }
 
-    @Test
-    void createsAddressDto__ifStreetLengthOk() {
-        String street = RandomStringUtils.random(128, true, true);
-        var addressDto = new CovidCertificateAddressDto(street, 1000, "city", "BE");
-        assertDoesNotThrow(addressDto::validate);
+    @Nested
+    @Tag("CovidCertificateAddressDto.cantonCodeSender")
+    @DisplayName("'cantonCodeSender' must be a valid canton code or MI.")
+    class CantonCodeSenderTests {
+
+        @ParameterizedTest
+        @ValueSource(strings = {"AG", "AI", "AR", "BE", "BL", "BS", "FR", "GE", "GL", "GR", "JU", "LU", "NE", "NW", "OW", "SG", "SH", "SO", "SZ", "TG", "TI", "UR", "VD", "VS", "ZG", "ZH", "MI"})
+        @DisplayName("Given 'cantonCodeSender' is within the value set, when validated, it should not throw an exception.")
+        void cantonCodeSenderTest1(String cantonCodeSender) {
+            var covidCertificateAddressDto = new CovidCertificateAddressDto("streetAndNr", MIN_ZIPCODE_VALUE, "city", cantonCodeSender);
+            assertDoesNotThrow(covidCertificateAddressDto::validate);
+        }
+
+        @ParameterizedTest
+        @NullAndEmptySource
+        @ValueSource(strings = {" ", "ZZ"})
+        @DisplayName("Given 'cantonCodeSender' is blank or not in the value set, when validated, it should throw an INVALID_ADDRESS error (CreateCertificateException).")
+        void cantonCodeSenderTest2(String cantonCodeSender) {
+            var covidCertificateAddressDto = new CovidCertificateAddressDto("streetAndNr", MIN_ZIPCODE_VALUE, "city", cantonCodeSender);
+            assertInvalidAddressError(covidCertificateAddressDto);
+        }
     }
 
-    @Test
-    void throwsCertificateCreateException__onEmptyCity() {
-        var addressDto = new CovidCertificateAddressDto("streetAndNr", 1000, null, "BE");
-        CreateCertificateException exception = assertThrows(CreateCertificateException.class, addressDto::validate);
-        assertEquals(INVALID_ADDRESS, exception.getError());
+    @Nested
+    @Tag("CovidCertificateAddressDto.streetAndNr")
+    @DisplayName("'streetAndNr' can't be blank and can't exceed " + MAX_STREETANDNR_CHARS_LENGTH + " characters.")
+    class StreetAndNumberTests {
 
-        addressDto = new CovidCertificateAddressDto("streetAndNr", 1000, "", "BE");
-        exception = assertThrows(CreateCertificateException.class, addressDto::validate);
-        assertEquals(INVALID_ADDRESS, exception.getError());
+        @ParameterizedTest
+        @NullAndEmptySource
+        @ValueSource(strings = {" "})
+        @DisplayName("Given 'streetAndNr' is blank, when validated, it should throw an INVALID_ADDRESS error (CreateCertificateException).")
+        void streetAndNumberTest1(String streetAndNr) {
+            var covidCertificateAddressDto = new CovidCertificateAddressDto(streetAndNr, MIN_ZIPCODE_VALUE, "city", "BE");
+            assertInvalidAddressError(covidCertificateAddressDto);
+        }
 
-        addressDto = new CovidCertificateAddressDto("streetAndNr", 1000, "   ", "BE");
-        exception = assertThrows(CreateCertificateException.class, addressDto::validate);
-        assertEquals(INVALID_ADDRESS, exception.getError());
+        @ParameterizedTest
+        @ValueSource(ints = {MAX_STREETANDNR_CHARS_LENGTH - 1, MAX_STREETANDNR_CHARS_LENGTH})
+        @DisplayName("Given 'streetAndNr' length <= " + MAX_STREETANDNR_CHARS_LENGTH + ", when validated, it should not throw an exception.")
+        void streetAndNumberTest2(int streetAndNrLength) {
+            String streetAndNumber = RandomStringUtils.random(streetAndNrLength, true, true);
+            var covidCertificateAddressDto = new CovidCertificateAddressDto(streetAndNumber, MIN_ZIPCODE_VALUE, "city", "BE");
+            assertDoesNotThrow(covidCertificateAddressDto::validate);
+        }
+
+        @Test
+        @DisplayName("Given 'streetAndNr' length > " + MAX_STREETANDNR_CHARS_LENGTH + " characters, when validated, it should throw an INVALID_ADDRESS error (CreateCertificateException).")
+        void streetAndNumberTest3() {
+            String streetAndNumber = RandomStringUtils.random(MAX_STREETANDNR_CHARS_LENGTH + 1, true, true);
+            var covidCertificateAddressDto = new CovidCertificateAddressDto(streetAndNumber, MIN_ZIPCODE_VALUE, "city", "BE");
+            assertInvalidAddressError(covidCertificateAddressDto);
+        }
     }
 
-    @Test
-    void throwsCertificateCreateException__ifCityTooLong() {
-        String city = RandomStringUtils.random(129, true, true);
-        var addressDto = new CovidCertificateAddressDto("streetAndNr", 1000, city, "BE");
-        CreateCertificateException exception = assertThrows(CreateCertificateException.class, addressDto::validate);
-        assertEquals(INVALID_ADDRESS, exception.getError());
+    @Nested
+    @Tag("CovidCertificateAddressDto.zipCode")
+    @DisplayName("'zipCode' must be contained in the [" + MIN_ZIPCODE_VALUE + ", " + MAX_ZIPCODE_VALUE + "] interval.")
+    class ZipCodeTests {
+
+        @ParameterizedTest
+        @ValueSource(ints = {MIN_ZIPCODE_VALUE - 1, MAX_ZIPCODE_VALUE + 1})
+        @DisplayName("Given 'zipCode' is outside of the [" + MIN_ZIPCODE_VALUE + ", " + MAX_ZIPCODE_VALUE + "] interval, when validated, it should throw an INVALID_ADDRESS error (CreateCertificateException).")
+        void zipCodeTest1(int zipCode) {
+            var covidCertificateAddressDto = new CovidCertificateAddressDto("streetAndNr", zipCode, "city", "BE");
+            assertInvalidAddressError(covidCertificateAddressDto);
+        }
+
+        @ParameterizedTest
+        @ValueSource(ints = {MIN_ZIPCODE_VALUE, MAX_ZIPCODE_VALUE})
+        @DisplayName("Given 'zipCode' is equal to the [" + MIN_ZIPCODE_VALUE + ", " + MAX_ZIPCODE_VALUE + "] interval limits, when validated, it should not throw an exception.")
+        void zipCodeTest2(int zipCode) {
+            var covidCertificateAddressDto = new CovidCertificateAddressDto("streetAndNr", zipCode, "city", "BE");
+            assertDoesNotThrow(covidCertificateAddressDto::validate);
+        }
+
+        @ParameterizedTest
+        @ValueSource(ints = {MIN_ZIPCODE_VALUE + 1, MAX_ZIPCODE_VALUE - 1})
+        @DisplayName("Given 'zipCode' is contained in the [" + MIN_ZIPCODE_VALUE + ", " + MAX_ZIPCODE_VALUE + "] interval, when validated, it should not throw an exception.")
+        void zipCodeTest3(int zipCode) {
+            var covidCertificateAddressDto = new CovidCertificateAddressDto("streetAndNr", zipCode, "city", "BE");
+            assertDoesNotThrow(covidCertificateAddressDto::validate);
+        }
     }
 
-    @Test
-    void createsAddressDto__ifCityLengthOk() {
-        String city = RandomStringUtils.random(128, true, true);
-        var addressDto = new CovidCertificateAddressDto("streetAndNr", 1000, city, "BE");
-        assertDoesNotThrow(addressDto::validate);
-    }
+    @Nested
+    @Tag("CovidCertificateAddressDtoTest.city")
+    @DisplayName("'city' can't be blank and can't exceed " + MAX_CITY_CHARS_LENGTH + " characters lenght.")
+    class CityTests {
 
-    @Test
-    void throwsCertificateCreateException__onInvalidZipCode() {
-        // test low
-        var addressDto = new CovidCertificateAddressDto("streetAndNr", 999, "city", "BE");
-        CreateCertificateException exception = assertThrows(CreateCertificateException.class, addressDto::validate);
-        assertEquals(INVALID_ADDRESS, exception.getError());
+        @ParameterizedTest
+        @NullAndEmptySource
+        @ValueSource(strings = {" "})
+        @DisplayName("Given 'city' is blank, when validated, it should throw an INVALID_ADDRESS error (CreateCertificateException).")
+        void cityTest1(String city) {
+            var covidCertificateAddressDto = new CovidCertificateAddressDto("streetAndNr", MIN_ZIPCODE_VALUE, city, "BE");
+            assertInvalidAddressError(covidCertificateAddressDto);
+        }
 
-        // test high
-        addressDto = new CovidCertificateAddressDto("streetAndNr", 10000, "city", "BE");
-        exception = assertThrows(CreateCertificateException.class, addressDto::validate);
-        assertEquals(INVALID_ADDRESS, exception.getError());
-    }
+        @ParameterizedTest
+        @ValueSource(ints = {MAX_CITY_CHARS_LENGTH - 1, MAX_CITY_CHARS_LENGTH})
+        @DisplayName("Given 'city' length <= " + MAX_CITY_CHARS_LENGTH + " characters, when validated, it should not throw an exception.")
+        void cityTest2(int cityLength) {
+            String city = RandomStringUtils.random(cityLength, true, true);
+            var covidCertificateAddressDto = new CovidCertificateAddressDto("streetAndNr", MIN_ZIPCODE_VALUE, city, "BE");
+            assertDoesNotThrow(covidCertificateAddressDto::validate);
+        }
 
-    @Test
-    void createsAddressDto__onZipCodeValid() {
-        // test low
-        var addressDto = new CovidCertificateAddressDto("streetAndNr", 1000, "city", "BE");
-        assertDoesNotThrow(addressDto::validate);
-
-        // test high
-        addressDto = new CovidCertificateAddressDto("streetAndNr", 9999, "city", "BE");
-        assertDoesNotThrow(addressDto::validate);
-    }
-
-    @Test
-    void throwsCertificateCreateException__onInvalidCantonCode() {
-        try (MockedStatic<AllowedSenders> allowedCantonsMock = Mockito.mockStatic(AllowedSenders.class)) {
-            var addressDto = new CovidCertificateAddressDto("streetAndNr", 1000, "city", "TT");
-            allowedCantonsMock.when(() -> AllowedSenders.isAccepted(any())).thenReturn(false);
-            CreateCertificateException exception = assertThrows(CreateCertificateException.class, addressDto::validate);
-            assertEquals(INVALID_ADDRESS, exception.getError());
-            allowedCantonsMock.verify(times(1), () -> AllowedSenders.isAccepted(any()));
+        @Test
+        @DisplayName("Given 'city' length > " + MAX_STREETANDNR_CHARS_LENGTH + " characters, when validated, it should throw an INVALID_ADDRESS error (CreateCertificateException).")
+        void cityTest3() {
+            String city = RandomStringUtils.random(MAX_STREETANDNR_CHARS_LENGTH + 1, true, true);
+            var covidCertificateAddressDto = new CovidCertificateAddressDto("streetAndNr", MIN_ZIPCODE_VALUE, city, "BE");
+            assertInvalidAddressError(covidCertificateAddressDto);
         }
     }
 }
