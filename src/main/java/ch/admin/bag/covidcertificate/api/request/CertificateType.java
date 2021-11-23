aed9509ce92e28058af8b1334a9952a4302b46a4
mofobo <mohammed.fonsecaboukhalat@bit.admin.ch>
2021-11-23 10:44:40 +0100

    add string to CertificateType

--- src/main/java/ch/admin/bag/covidcertificate/api/request/CertificateType.java
+++ src/main/java/ch/admin/bag/covidcertificate/api/request/CertificateType.java
@@ -1,5 +1,28 @@
 package ch.admin.bag.covidcertificate.api.request;
 
 public enum CertificateType {
-    recovery, test, vaccination, vaccination_tourist, antibody
+    recovery("recovery"), test("test"), vaccination("vaccination"), vaccination_tourist("vaccination-tourist"), antibody("antibody");
+
+    private final String text;
+
+    /**
+     * @param text
+     */
+    CertificateType(final String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return text;
+    }
+
+    public static CertificateType fromString(String text) {
+        for (CertificateType certificateType : CertificateType.values()) {
+            if (certificateType.text.equalsIgnoreCase(text)) {
+                return certificateType;
+            }
+        }
+        return null;
+    }
 }
