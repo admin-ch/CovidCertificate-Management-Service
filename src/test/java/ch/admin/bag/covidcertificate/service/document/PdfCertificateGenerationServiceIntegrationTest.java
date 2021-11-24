9dc60530e852881f727da9710e9c0cf7da145d27
Georgios Papadopoulos <papadopoulos.georgios@bit.admin.ch>
2021-11-24 14:05:33 +0100

    VACCINECER-1688 improve unit tests for pdfRenderer and pdfGenerationService

--- src/test/java/ch/admin/bag/covidcertificate/service/document/PdfCertificateGenerationServiceIntegrationTest.java
+++ src/test/java/ch/admin/bag/covidcertificate/service/document/PdfCertificateGenerationServiceIntegrationTest.java
@@ -13,7 +13,6 @@ import ch.admin.bag.covidcertificate.api.request.VaccinationCertificateCreateDto
 import ch.admin.bag.covidcertificate.api.valueset.IssuableTestDto;
 import ch.admin.bag.covidcertificate.api.valueset.IssuableVaccineDto;
 import ch.admin.bag.covidcertificate.api.valueset.TestType;
-import ch.admin.bag.covidcertificate.service.document.util.PdfHtmlRenderer;
 import ch.admin.bag.covidcertificate.service.domain.AbstractCertificatePdf;
 import ch.admin.bag.covidcertificate.service.domain.RecoveryCertificatePdf;
 import ch.admin.bag.covidcertificate.service.domain.RecoveryCertificateQrCode;
@@ -22,48 +21,45 @@ import ch.admin.bag.covidcertificate.service.domain.TestCertificateQrCode;
 import ch.admin.bag.covidcertificate.service.domain.VaccinationCertificatePdf;
 import ch.admin.bag.covidcertificate.service.domain.VaccinationCertificateQrCode;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.stream.Stream;
 
 import static ch.admin.bag.covidcertificate.TestModelProvider.getRecoveryCertificateCreateDto;
 import static ch.admin.bag.covidcertificate.TestModelProvider.getTestCertificateCreateDto;
 import static ch.admin.bag.covidcertificate.TestModelProvider.getVaccinationCertificateCreateDto;
 
-@ExtendWith(MockitoExtension.class)
-class CovidPdfCertificateGenerationServiceTest {
+class PdfCertificateGenerationServiceIntegrationTest {
 
-    private PdfCertificateGenerationService service;
+    private final PdfCertificateGenerationService service = new PdfCertificateGenerationService();
 
     private final String countryEn = "Switzerland";
 
-    private final String familyNameBig = "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW";
-    private final String givenNameBig = "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW";
+    private static final String familyNameBig = "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW";
+    private static final String givenNameBig = "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW";
 
-    private final String familyNameMiddle = "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW";
-    private final String givenNameMiddle = "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW";
+    private static final String familyNameMiddle = "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW";
+    private static final String givenNameMiddle = "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW";
 
-    private final String familyNameSmall = "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW";
-    private final String givenNameSmall = "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW";
-
+    private static final String familyNameSmall = "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW";
+    private static final String givenNameSmall = "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW";
 
     @BeforeEach
     void setup() {
-        service = new PdfCertificateGenerationService();
         ReflectionTestUtils.setField(service, "showWatermark", true);
     }
 
     private void generateDocument_vaccine(VaccinationCertificateCreateDto createDto, String language, String familyName, String givenName, String fileName) throws Exception {
         IssuableVaccineDto vaccineDto = new IssuableVaccineDto("EU/1/20/1528", "Comirnaty", "1119349007",
                 "SARS-CoV-2 mRNA vaccine", "ORG-100030215",
-                "Biontech Manufacturing GmbH", Issuable.CH_ONLY, true);
+                "Biontech Manufacturing GmbH", Issuable.CH_ONLY, false);
         ReflectionTestUtils.setField(createDto.getPersonData().getName(), "familyName", familyName);
         ReflectionTestUtils.setField(createDto.getPersonData().getName(), "givenName", givenName);
         String country = "Schweiz";
@@ -117,39 +113,36 @@ class CovidPdfCertificateGenerationServiceTest {
 
     }
 
-    @Test
-    void generateAllDocuments() throws Exception {
-        generateAllDocuments(familyNameSmall, givenNameSmall);
-        generateAllDocuments(familyNameMiddle, givenNameMiddle);
-        generateAllDocuments(familyNameBig, givenNameBig);
-
+    @ParameterizedTest
+    @MethodSource("testConfiguration")
+    void generateRecoverylDocuments(String language, String familyName, String givenName) throws Exception {
+        generateDocument_recovery(language, familyName, givenName);
     }
 
-    private void generateAllDocuments(String familyName, String givenName) throws Exception {
-        generateDocument_recovery("de", familyName, givenName);
-        generateDocument_recovery("fr", familyName, givenName);
-        generateDocument_recovery("it", familyName, givenName);
-        generateDocument_recovery("rm", familyName, givenName);
+    @ParameterizedTest
+    @MethodSource("testConfiguration")
+    void generateVaccinationDocuments(String language, String familyName, String givenName) throws Exception {
+        generateDocument_vaccine(language, familyName, givenName);
+    }
 
-        generateDocument_vaccine("de", familyName, givenName);
-        generateDocument_vaccine("fr", familyName, givenName);
-        generateDocument_vaccine("it", familyName, givenName);
-        generateDocument_vaccine("rm", familyName, givenName);
 
-        generateDocument_test("de", familyName, givenName);
-        generateDocument_test("fr", familyName, givenName);
-        generateDocument_test("it", familyName, givenName);
-        generateDocument_test("rm", familyName, givenName);
+    @ParameterizedTest
+    @MethodSource("testConfiguration")
+    void generatePartialVaccinationDocuments(String language, String familyName, String givenName) throws Exception {
+        generateDocument_partialVaccination(language, familyName, givenName);
+    }
 
-        generateDocument_partialVaccination("de", familyName, givenName);
-        generateDocument_partialVaccination("fr", familyName, givenName);
-        generateDocument_partialVaccination("it", familyName, givenName);
-        generateDocument_partialVaccination("rm", familyName, givenName);
+
+    @ParameterizedTest
+    @MethodSource("testConfiguration")
+    void generateTestDocuments(String language, String familyName, String givenName) throws Exception {
+        generateDocument_test(language, familyName, givenName);
     }
 
     void doTest(AbstractCertificatePdf pdfData, String filename, String language) throws Exception {
 
         var barcodePayload = "HC1:NCFOXNYTSFDHJI8-.O0:A%1W RI%.BI06%BF1WG21QKP85NPV*JVH5QWKIW18WA%NE/P3F/8X*G3M9FQH+4JZW4V/AY73CIBVQFSA36238FNB939PJ*KN%DJ3239L7BRNHKBWINEV40AT0C7LS4AZKZ73423ZQT-EJEG3LS4JXITAFK1HG%8SC91Z8YA7-TIP+PQE1W9L $N3-Q-*OGF2F%M RFUS2CPA-DG:A3AGJLC1788M7DD-I/2DBAJDAJCNB-439Y4.$SINOPK3.T4RZ4E%5MK9QM9DB9E%5:I9YHQ1FDIV4RB4VIOTNPS46UDBQEAJJKHHGQA8EL4QN9J9E6LF6JC1A5N11+N1X*8O13E20ZO8%3";
+
 
         byte[] document = service.generateCovidCertificate(pdfData, barcodePayload, LocalDateTime.now());
 
@@ -160,5 +153,14 @@ class CovidPdfCertificateGenerationServiceTest {
             out.write(document);
             out.close();
         }
+    }
+
+    private static Stream<Arguments> testConfiguration() {
+        return Stream.of("de", "fr", "it", "rm").
+                flatMap(language -> Stream.of(
+                        Arguments.of(language, familyNameSmall, givenNameSmall),
+                        Arguments.of(language, familyNameMiddle, givenNameMiddle),
+                        Arguments.of(language, familyNameBig, givenNameBig)
+                ));
     }
 }
---
+++ src/test/java/ch/admin/bag/covidcertificate/service/document/PdfCertificateGenerationServiceTest.java
@@ -0,0 +1,78 @@
+package ch.admin.bag.covidcertificate.service.document;
+
+import ch.admin.bag.covidcertificate.service.document.util.PdfHtmlRenderer;
+import ch.admin.bag.covidcertificate.service.domain.AbstractCertificatePdf;
+import ch.admin.bag.covidcertificate.service.domain.AntibodyCertificatePdf;
+import ch.admin.bag.covidcertificate.service.domain.RecoveryCertificatePdf;
+import ch.admin.bag.covidcertificate.service.domain.TestCertificatePdf;
+import ch.admin.bag.covidcertificate.service.domain.VaccinationCertificatePdf;
+import ch.admin.bag.covidcertificate.service.domain.VaccinationTouristCertificatePdf;
+import com.flextrade.jfixture.JFixture;
+import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.MockedConstruction;
+import org.mockito.Mockito;
+
+import java.time.LocalDateTime;
+
+import static ch.admin.bag.covidcertificate.FixtureCustomization.customizeAntibodyCertificatePdf;
+import static ch.admin.bag.covidcertificate.FixtureCustomization.customizeRecoveryCertificatePdf;
+import static ch.admin.bag.covidcertificate.FixtureCustomization.customizeTestCertificatePdf;
+import static ch.admin.bag.covidcertificate.FixtureCustomization.customizeVaccinationCertificatePdf;
+import static ch.admin.bag.covidcertificate.FixtureCustomization.customizeVaccinationTouristCertificatePdf;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+
+class PdfCertificateGenerationServiceTest {
+
+    private final PdfCertificateGenerationService service = new PdfCertificateGenerationService();
+
+    private final JFixture fixture = new JFixture();
+
+    @BeforeEach
+    void setup() {
+        customizeVaccinationCertificatePdf(fixture);
+        customizeTestCertificatePdf(fixture);
+        customizeRecoveryCertificatePdf(fixture);
+        customizeAntibodyCertificatePdf(fixture);
+        customizeVaccinationTouristCertificatePdf(fixture);
+    }
+
+    @Nested
+    class GenerateCovidCertificate {
+        @ParameterizedTest
+        @ValueSource(classes = {VaccinationCertificatePdf.class, TestCertificatePdf.class, RecoveryCertificatePdf.class, AntibodyCertificatePdf.class, VaccinationTouristCertificatePdf.class})
+        void shouldCreateANewPdfRendererAtEachCall(Class<? extends AbstractCertificatePdf> clazz) {
+            try (MockedConstruction<PdfHtmlRenderer> pdfHtmlRendererMockConstructor = Mockito.mockConstruction(PdfHtmlRenderer.class,
+                    (mock, context) -> when(mock.render(any(), any(), any())).thenReturn(fixture.create(String.class)));
+                 MockedConstruction<PdfRendererBuilder> pdfRendererBuilderMockedConstruction = Mockito.mockConstruction(PdfRendererBuilder.class,
+                         (mock, context) -> doNothing().when(mock).run())) {
+
+                service.generateCovidCertificate(fixture.create(clazz), fixture.create(String.class), LocalDateTime.now());
+                service.generateCovidCertificate(fixture.create(clazz), fixture.create(String.class), LocalDateTime.now());
+
+                assertEquals(2, pdfHtmlRendererMockConstructor.constructed().size());
+            }
+        }
+
+        @ParameterizedTest
+        @ValueSource(classes = {VaccinationCertificatePdf.class, TestCertificatePdf.class, RecoveryCertificatePdf.class, AntibodyCertificatePdf.class, VaccinationTouristCertificatePdf.class})
+        void shouldCreateANewPdfRendererBuilderAtEachCall(Class<? extends AbstractCertificatePdf> clazz) {
+            try (MockedConstruction<PdfHtmlRenderer> pdfHtmlRendererMockConstructor = Mockito.mockConstruction(PdfHtmlRenderer.class,
+                    (mock, context) -> when(mock.render(any(), any(), any())).thenReturn(fixture.create(String.class)));
+                 MockedConstruction<PdfRendererBuilder> pdfRendererBuilderMockedConstruction = Mockito.mockConstruction(PdfRendererBuilder.class,
+                         (mock, context) -> doNothing().when(mock).run())) {
+
+                service.generateCovidCertificate(fixture.create(clazz), fixture.create(String.class), LocalDateTime.now());
+                service.generateCovidCertificate(fixture.create(clazz), fixture.create(String.class), LocalDateTime.now());
+
+                assertEquals(2, pdfRendererBuilderMockedConstruction.constructed().size());
+            }
+        }
+    }
+}
