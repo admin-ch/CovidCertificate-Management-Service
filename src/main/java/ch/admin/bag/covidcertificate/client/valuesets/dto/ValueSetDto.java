400f98e42805efb42d62abc8e8066d8f00634bcc
mofobo <mohammed.fonsecaboukhalat@bit.admin.ch>
2021-12-07 17:53:38 +0100

    improve import

--- src/main/java/ch/admin/bag/covidcertificate/client/valuesets/dto/ValueSetDto.java
+++ src/main/java/ch/admin/bag/covidcertificate/client/valuesets/dto/ValueSetDto.java
@@ -1,6 +1,13 @@
 package ch.admin.bag.covidcertificate.client.valuesets.dto;
 
-import lombok.*;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+
 
 @Getter
 @ToString
