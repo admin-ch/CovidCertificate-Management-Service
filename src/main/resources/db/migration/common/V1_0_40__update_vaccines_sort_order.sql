878ed14e1862c90b74c957dca41b2550ddc8eb71
Harald Lösing <harald.loesing@bit.admin.ch>
2022-01-18 18:16:31 +0100

    fixed signing key for local development, fixed sort order of vaccines.

---
+++ src/main/resources/db/migration/common/V1_0_40__update_vaccines_sort_order.sql
@@ -0,0 +1,6 @@
+update vaccines_covid_19_names
+set vaccine_order = 58
+where id in (
+             '2dc3feef-95d8-4e6f-930c-69300e33e6f0',
+             '2dc3feef-95d8-4e6f-930c-69300e33f7f0'
+    );
