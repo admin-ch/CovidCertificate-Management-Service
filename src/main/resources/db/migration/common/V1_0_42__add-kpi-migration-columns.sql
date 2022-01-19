2a9c1e81b6a5dfe6153c74e56ea8f57cc7dcd7af
Iris Hunkeler <iris.hunkeler@bit.admin.ch>
2022-01-19 14:50:06 +0100

    SAVINT-1702 KPI migration: remove update statement from flyway

--- src/main/resources/db/migration/common/V1_0_42__add-kpi-migration-columns.sql
+++ src/main/resources/db/migration/common/V1_0_42__add-kpi-migration-columns.sql
@@ -1,9 +1,6 @@
 ALTER TABLE kpi
     ADD COLUMN IF NOT EXISTS system_source VARCHAR(3);
 
-UPDATE kpi
-    SET system_source = 'UI';
-
 ALTER TABLE kpi
     ADD COLUMN IF NOT EXISTS api_gateway_id VARCHAR(36);
 
