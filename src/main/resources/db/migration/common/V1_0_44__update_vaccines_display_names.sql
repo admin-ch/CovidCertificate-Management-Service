65aaa36ee878eecd6cd657b33fb3cb23427ff6ab
Harald Lösing <harald.loesing@bit.admin.ch>
2022-01-21 14:59:42 +0100

    update vaccines to eu version 2.5.0, display names.

---
+++ src/main/resources/db/migration/common/V1_0_44__update_vaccines_display_names.sql
@@ -0,0 +1,12 @@
+update vaccines_covid_19_names
+set display = 'BBIBP-CorV'
+where id = '4109a8fa-84b2-4a3f-bf33-6c746a79a11d';
+update vaccines_covid_19_names
+set display = 'BBIBP-CorV'
+where id = '4109a8fa-84b2-4a3f-bf33-6c746a79b21d';
+update vaccines_covid_19_names
+set display = 'CoronaVac'
+where id = 'fe39b8af-eccd-45de-acd7-ce7206052af7';
+update vaccines_covid_19_names
+set display = 'CoronaVac'
+where id = 'fe39b8af-eccd-45de-acd7-ce7206053bf7';
