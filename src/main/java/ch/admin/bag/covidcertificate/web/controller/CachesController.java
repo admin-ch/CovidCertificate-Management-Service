3df2735613583a22bdcdc2a998dbf134a5ff3d38
Harald Lösing <harald.loesing@bit.admin.ch>
2022-02-24 16:53:45 +0100

    remove strong auth verification, fix test, change role check in caches controller.

--- src/main/java/ch/admin/bag/covidcertificate/web/controller/CachesController.java
+++ src/main/java/ch/admin/bag/covidcertificate/web/controller/CachesController.java
@@ -33,7 +33,7 @@ public class CachesController {
      * @param only OPTIONAL list or single String seperated by "," of caches
      */
     @PostMapping("/clear")
-    @PreAuthorize("hasAnyRole('bag-cc-superuser')")
+    @PreAuthorize("hasRole('bag-cc-superuser')")
     public void clear(@RequestParam(required = false) Optional<List<String>> only) {
         log.info("Call of clear all caches.");
 
