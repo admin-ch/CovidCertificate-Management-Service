c17677ce2ef48e9bc032eb8a88ed6bf258539d7c
Harald Lösing <harald.loesing@bit.admin.ch>
2022-07-01 15:45:32 +0200

    sampleDateTime gets now parsed with and without time to map RecoveryRatCertificateCsvBean to a valid RecoveryRatCertificateDataDto.

--- src/main/java/ch/admin/bag/covidcertificate/api/request/RecoveryRatCertificateCsvBean.java
+++ src/main/java/ch/admin/bag/covidcertificate/api/request/RecoveryRatCertificateCsvBean.java
@@ -7,15 +7,20 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 
 import static ch.admin.bag.covidcertificate.api.Constants.INVALID_SAMPLE_DATE_TIME;
+import static ch.admin.bag.covidcertificate.api.Constants.SWISS_TIMEZONE;
 
 @Getter
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
 public class RecoveryRatCertificateCsvBean extends CertificateCreateCsvBean {
+
+    public static final String TIME = "T";
 
     @CsvBindByName(column = "sampleDateTime")
     private String sampleDateTime;
@@ -26,7 +31,20 @@ public class RecoveryRatCertificateCsvBean extends CertificateCreateCsvBean {
     public RecoveryRatCertificateCreateDto mapToCreateDto() {
         ZonedDateTime sampleDateTimeParsed;
         try {
-            sampleDateTimeParsed = ZonedDateTime.parse(this.sampleDateTime);
+            if (this.sampleDateTime.contains(TIME)) {
+                // it contains a time
+                if (this.sampleDateTime.contains("Z")) {
+                    // it is UTC zoned
+                    ZonedDateTime utcZoned = ZonedDateTime.parse(this.sampleDateTime);
+                    sampleDateTimeParsed = utcZoned.withZoneSameInstant(SWISS_TIMEZONE);
+                } else {
+                    // it is un zoned and we interpret it as SWISS_TIMEZONE
+                    sampleDateTimeParsed = LocalDateTime.parse(this.sampleDateTime).atZone(SWISS_TIMEZONE);
+                }
+            } else {
+                // it is without time and we take start of day with SWISS_TIMEZONE
+                sampleDateTimeParsed = LocalDate.parse(sampleDateTime).atStartOfDay(SWISS_TIMEZONE);
+            }
         } catch (Exception e) {
             throw new CreateCertificateException(INVALID_SAMPLE_DATE_TIME);
         }
