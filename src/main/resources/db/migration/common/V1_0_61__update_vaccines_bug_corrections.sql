40e7213bd9ca9434323b35379fcd08f93fb776a0
Harald Lösing <harald.loesing@bit.admin.ch>
2022-08-19 15:23:08 +0200

    change code of auth holder Valneva France to ORG-100036422, delete old ch_only declaration of 'Convidecia' from vaccine names and prepare next release 4.5.4.

---
+++ src/main/resources/db/migration/common/V1_0_61__update_vaccines_bug_corrections.sql
@@ -0,0 +1,7 @@
+UPDATE vaccines_covid_19_auth_holders
+set code = 'ORG-100036422'
+where id = 'ed9c2a60-4b8e-4b1c-9370-d4df32781057';
+
+DELETE
+FROM vaccines_covid_19_names
+where id = 'bc0e71cf-bc52-45cb-b6f0-1dba39cf2a97';
