985ade71f36d978d752b335b17aa97ccd87486e1
Harald Lösing <harald.loesing@bit.admin.ch>
2022-09-06 15:58:10 +0200

    Formatted AntibodyCertificateCreateCsvBeanTest to IDE standard.

--- src/test/java/ch/admin/bag/covidcertificate/api/request/AntibodyCertificateCreateCsvBeanTest.java
+++ src/test/java/ch/admin/bag/covidcertificate/api/request/AntibodyCertificateCreateCsvBeanTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 class AntibodyCertificateCreateCsvBeanTest {
 
     @Test
-    void mapToCreateDto_validFields_dtoCreated(){
+    void mapToCreateDto_validFields_dtoCreated() {
         AntibodyCertificateCsvBean antibodyCertificateCsvBean = new AntibodyCertificateCsvBean("2022-04-30", "Dr. Strange Praxis");
         ReflectionTestUtils.setField(antibodyCertificateCsvBean, "familyName", "test");
         ReflectionTestUtils.setField(antibodyCertificateCsvBean, "givenName", "test");
@@ -18,7 +18,7 @@ class AntibodyCertificateCreateCsvBeanTest {
     }
 
     @Test
-    void mapToCreateDto_nullFields_dtoCreated(){
+    void mapToCreateDto_nullFields_dtoCreated() {
         AntibodyCertificateCsvBean antibodyCertificateCsvBean = new AntibodyCertificateCsvBean("2022-04-30", null);
         ReflectionTestUtils.setField(antibodyCertificateCsvBean, "familyName", "test");
         ReflectionTestUtils.setField(antibodyCertificateCsvBean, "givenName", "test");
