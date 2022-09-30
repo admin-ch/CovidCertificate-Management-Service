96fb0e1fc936fa34d585533e8903d67f8a7ec327
Harald Lösing <harald.loesing@bit.admin.ch>
2022-09-30 09:03:49 +0200

    Created new vaccine EU/1/21/1624 and updated VLA2001 to active false and deprecated.

---
+++ src/main/resources/db/migration/local/V1_0_67__update_signing_information_EU_2.9.0.sql
@@ -0,0 +1,17 @@
+INSERT INTO signing_information
+( id
+, certificate_type
+, code
+, alias
+, certificate_alias
+, valid_from
+, valid_to
+, slot_number)
+VALUES ( '7cac86f1-ada1-4fa2-98b6-278e06385445'
+       , 'vaccination'
+       , 'EU/1/21/1624'
+       , 'mock'
+       , 'mock'
+       , '2022-09-30'
+       , '2999-12-31'
+       , 0);
