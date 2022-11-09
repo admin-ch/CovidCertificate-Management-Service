e8483945a453584902a8638b19870a09b25bc2c1
Harald Lösing <harald.loesing@bit.admin.ch>
2022-11-09 09:59:08 +0100

    Removed function for uuid generation because of failing tests, prepared next release.

--- src/main/resources/db/migration/common/V1_0_72__maintain_overwritten_texts_for_Vaccines.sql
+++ src/main/resources/db/migration/common/V1_0_72__maintain_overwritten_texts_for_Vaccines.sql
@@ -2,7 +2,7 @@ insert into display_name_modification ( id
                                       , code
                                       , display
                                       , entity_type)
-values ( uuid_in(md5(random()::text || clock_timestamp()::text)::cstring)
+values ( 'de6e4c65-ae1f-85ec-4cd6-7ed62db56e9f'
        , 'BBIBP-CorV'
        , 'BBIBP-CorV / Covilo'
        , 'Vaccine');
@@ -11,7 +11,7 @@ insert into display_name_modification ( id
                                       , code
                                       , display
                                       , entity_type)
-values ( uuid_in(md5(random()::text || clock_timestamp()::text)::cstring)
+values ( '9f657e7e-840d-7067-33f2-0255f2a043a9'
        , 'Covaxin'
        , 'Covaxin (also known as BBV152 A, B, C)'
        , 'Vaccine');
@@ -20,7 +20,7 @@ insert into display_name_modification ( id
                                       , code
                                       , display
                                       , entity_type)
-values ( uuid_in(md5(random()::text || clock_timestamp()::text)::cstring)
+values ( '06dcb1b5-ad07-a0d2-e7d4-17b4df4e4fb2'
        , 'Covishield'
        , 'Covishield (ChAdOx1_nCoV-19)'
        , 'Vaccine');
@@ -29,7 +29,7 @@ insert into display_name_modification ( id
                                       , code
                                       , display
                                       , entity_type)
-values ( uuid_in(md5(random()::text || clock_timestamp()::text)::cstring)
+values ( '4c90b3b1-4c0d-8599-2679-5a198394b07e'
        , 'Covovax'
        , 'COVOVAX (Novavax formulation)'
        , 'Vaccine');
@@ -38,7 +38,7 @@ insert into display_name_modification ( id
                                       , code
                                       , display
                                       , entity_type)
-values ( uuid_in(md5(random()::text || clock_timestamp()::text)::cstring)
+values ( '0220d7dd-ffa8-7fe0-1ef4-9daa24f55c48'
        , 'EU/1/20/1525'
        , 'COVID-19 Vaccine Janssen'
        , 'Vaccine');
