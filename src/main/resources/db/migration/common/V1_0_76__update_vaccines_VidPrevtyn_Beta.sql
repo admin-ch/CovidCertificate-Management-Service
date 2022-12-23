51121109872630f76553482c9cdd4eb1df64387f
Harald Lösing <harald.loesing@bit.admin.ch>
2022-12-23 08:53:13 +0100

    New vaccine VidPrevtyn Beta based on import of 2.10.0.

---
+++ src/main/resources/db/migration/common/V1_0_76__update_vaccines_VidPrevtyn_Beta.sql
@@ -0,0 +1,13 @@
+INSERT INTO vaccines_covid_19_names (id, display, prophylaxis, auth_holder, active, modified_at, swiss_medic, emea,
+                                     who_eul, issuable, web_ui_selectable, api_gateway_selectable,
+                                     api_platform_selectable, code, vaccine_order, analog_vaccine, created_at)
+VALUES ('23da4499-0d5b-4397-8d1e-fe614c26b604', 'VidPrevtyn Beta', '1ee3559a-ed64-4062-a10e-baeded624ae8',
+        '2c606986-abac-4dcc-bb60-888901e8722b', true, '2022-12-22 00:00:01.866260',
+        false, true, true, 'ch_and_abroad', false, true, true, 'EU/1/21/1580', 46, null, '2022-12-22 00:00:01.866260');
+
+INSERT INTO vaccines_covid_19_names (id, display, prophylaxis, auth_holder, active, modified_at, swiss_medic, emea,
+                                     who_eul, issuable, web_ui_selectable, api_gateway_selectable,
+                                     api_platform_selectable, code, vaccine_order, analog_vaccine, created_at)
+VALUES ('23da4499-0d5b-4397-8d1e-fe614c26b614', 'VidPrevtyn Beta', '1ee3559a-ed64-4062-a10e-baeded624ae8',
+        '2c606986-abac-4dcc-bb60-888901e8722b', true, '2022-12-22 00:00:01.866260',
+        false, true, true, 'abroad_only', true, false, false, 'EU/1/21/1580', 46, null, '2022-12-22 00:00:01.866260');
