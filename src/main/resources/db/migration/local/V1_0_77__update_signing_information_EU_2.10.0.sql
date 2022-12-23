eef77ca303a671a7ea522f6de1199c43fc032670
Harald Lösing <harald.loesing@bit.admin.ch>
2022-12-23 10:32:24 +0100

    Added local signing information for "EU/1/21/1580", "VidPrevtyn Beta" and prepared next release.

---
+++ src/main/resources/db/migration/local/V1_0_77__update_signing_information_EU_2.10.0.sql
@@ -0,0 +1,17 @@
+INSERT INTO signing_information
+( id
+, certificate_type
+, code
+, alias
+, certificate_alias
+, valid_from
+, valid_to
+, slot_number)
+VALUES ( '7cac86f1-ada1-4fa2-98b6-278e06385665'
+       , 'vaccination'
+       , 'EU/1/21/1580'
+       , 'mock'
+       , 'mock'
+       , '2022-12-23'
+       , '2999-12-31'
+       , 0);
