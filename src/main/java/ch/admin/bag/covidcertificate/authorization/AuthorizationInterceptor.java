24389c521f1926261f640352d6e16420401fb975
Armin Isenring <armin.isenring@bit.admin.ch>
2023-01-25 16:30:21 +0100

    outsourcing authorization and integration of the corresponding library

--- src/main/java/ch/admin/bag/covidcertificate/authorization/AuthorizationInterceptor.java
+++ src/main/java/ch/admin/bag/covidcertificate/authorization/AuthorizationInterceptor.java
@@ -7,7 +7,6 @@ import ch.admin.bag.covidcertificate.config.security.authentication.JeapAuthenti
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.bouncycastle.util.Objects;
-import org.flywaydb.core.internal.util.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -15,7 +14,6 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
 
@@ -47,23 +45,11 @@ public class AuthorizationInterceptor implements HandlerInterceptor {
         }
 
         Set<String> rawRoles = authentication.getUserRoles();
-        boolean isHinUser = rawRoles.contains("bag-cc-hin-epr") || rawRoles.contains("bag-cc-hin");
-        boolean isHinCodeOrPersonal = rawRoles.contains("bag-cc-hincode") || rawRoles.contains("bag-cc-personal");
-        if (isHinUser && !isHinCodeOrPersonal) {
-            log.warn("HIN-User not allowed to use the application...");
-            log.warn("userroles: {}", rawRoles);
+        if (!authorizationService.isUserPermitted(rawRoles)) {
             throw new AuthorizationException(Constants.ACCESS_DENIED_FOR_HIN_WITH_CH_LOGIN);
         }
 
-        List<ServiceData.Function> functions = authorizationService.getDefinition("management")
-                .getFunctions()
-                .values()
-                .stream()
-                .filter(f -> StringUtils.hasText(f.getUri()))
-                .filter(f -> f.matchesUri(uri))
-                .filter(f -> f.matchesHttpMethod(request.getMethod()))
-                .filter(f -> f.isBetween(LocalDateTime.now()))
-                .toList();
+        List<ServiceData.Function> functions = authorizationService.identifyFunction(uri, request.getMethod());
 
         if (functions.isEmpty()) {
             throw new AuthorizationException(Constants.NO_FUNCTION_CONFIGURED, uri);
@@ -74,14 +60,13 @@ public class AuthorizationInterceptor implements HandlerInterceptor {
         }
 
         ServiceData.Function function = functions.get(0);
-        Set<String> roles = authorizationService.mapRawRoles(rawRoles);
 
         log.info("Verify function authorization: {}, {}, {}",
                 kv("clientId", clientId),
-                kv("roles", roles),
+                kv("roles", authorizationService.mapRawRoles(rawRoles)),
                 kv("function", function.getIdentifier()));
 
-        boolean isGranted = authorizationService.isGranted(roles, function);
+        boolean isGranted = authorizationService.isGranted(rawRoles, function);
 
         if (!isGranted) {
             throw new AuthorizationException(Constants.FORBIDDEN, uri);
