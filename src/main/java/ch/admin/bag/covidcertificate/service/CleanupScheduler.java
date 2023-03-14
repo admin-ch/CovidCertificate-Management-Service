2f4fe79b82c1ab4beb0598904f26993be334dfc7
Armin Isenring <armin.isenring@bit.admin.ch>
2023-03-14 21:59:11 +0100

    cleanup scheduler and service added

---
+++ src/main/java/ch/admin/bag/covidcertificate/service/CleanupScheduler.java
@@ -0,0 +1,108 @@
+package ch.admin.bag.covidcertificate.service;
+
+import ch.admin.bag.covidcertificate.config.cleanup.Cleanup;
+import ch.admin.bag.covidcertificate.config.cleanup.CleanupConfig;
+import ch.admin.bag.covidcertificate.service.CleanupService.CleaningEffort;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StopWatch;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@Component
+// run on cloudfoundry-instance 0 OR on local spring profile
+@ConditionalOnExpression("'${CF_INSTANCE_INDEX}'=='0' or {'local'}.contains('${SPRING_PROFILES_ACTIVE}')")
+public class CleanupScheduler {
+
+    private final CleanupService cleanupService;
+    private final CleanupConfig cleanupConfig;
+    private List<CleaningEffort> efforts = null;
+    private final int MAX_RETRIES = 3;
+
+
+    public CleanupScheduler(CleanupService cleanupService, CleanupConfig cleanupConfig) {
+        this.cleanupService = cleanupService;
+        this.cleanupConfig = cleanupConfig;
+
+        log.info("CLEANUP | scheduler active");
+    }
+
+    @Transactional
+    @Scheduled(cron = "${cc-management-service.cleanup.cron}")
+    public void processCleanup() {
+
+        log.debug("CLEANUP | Setup");
+        List<CleaningEffort> efforts = new ArrayList<>();
+        Map<String, Cleanup> spots = cleanupConfig.getSpots();
+        for (String databaseName : spots.keySet()) {
+            CleaningEffort effort = cleanupService.create(databaseName, spots.get(databaseName));
+            if (effort != null) {
+                efforts.add(effort);
+            }
+        }
+
+        final LocalDate toDeleteBeforeDate = LocalDate.now().minusYears(2);
+
+        log.info("CLEANUP | Start - records older than >{}<", toDeleteBeforeDate.format(DateTimeFormatter.ISO_DATE));
+
+        StopWatch stopWatch = new StopWatch();
+
+        for (CleaningEffort effort : efforts) {
+            int retry = 0;
+            long count = cleanupService.getCount(effort, toDeleteBeforeDate);
+            if (count == 0) {
+                log.info("CLEANUP | {} - no records to clean", effort.getName());
+            } else {
+                log.debug("CLEANUP | {} - cleaning {} records", effort.getName(), count);
+                stopWatch.start();
+                long allDeleted = 0L, deleted = -1L;
+                while (deleted != 0) {
+                    try {
+                        deleted = cleanupService.delete(effort, toDeleteBeforeDate, true);
+                        allDeleted += deleted;
+                    } catch (Exception e) {
+                        log.error("CLEANUP | {} - cleaning failed, retry #{}/{}; throwed '{}'", effort.getName(), ++retry, MAX_RETRIES, e);
+                        if (retry < MAX_RETRIES) {
+                            deleted = -1;
+                        }
+                    }
+                }
+                if (retry < MAX_RETRIES) {
+                    // make sure that fraction of a batch are deleted aswell
+                    deleted = cleanupService.delete(effort, toDeleteBeforeDate, false);
+                    allDeleted += deleted;
+                }
+                stopWatch.stop();
+
+                log.info("CLEANUP | {} - {}/{} records cleaned in {}", effort.getName(), allDeleted, count, readableMillis(stopWatch.getLastTaskTimeMillis()));
+            }
+        }
+        log.info("CLEANUP | End");
+
+        log.debug("CLEANUP | Tear down");
+        for (CleaningEffort effort : efforts) {
+            cleanupService.destroy(effort);
+        }
+    }
+
+    private static final DateTimeFormatter millisFormatter = DateTimeFormatter.ofPattern("'%d'.SSS's'");
+
+    private String readableMillis(long millis) {
+        LocalDateTime dateTime = Instant.ofEpochMilli(millis)
+                .atZone(ZoneId.systemDefault()) // default zone
+                .toLocalDateTime();
+        String format = dateTime.format(millisFormatter);
+        return String.format(format, dateTime.getMinute()*60 + dateTime.getSecond());
+    }
+}
---
+++ src/main/java/ch/admin/bag/covidcertificate/service/CleanupService.java
@@ -0,0 +1,153 @@
+package ch.admin.bag.covidcertificate.service;
+
+import ch.admin.bag.covidcertificate.config.cleanup.Cleanup;
+import ch.admin.bag.covidcertificate.config.cleanup.Database;
+import ch.admin.bag.covidcertificate.config.cleanup.SqlQuery;
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class CleanupService {
+
+    /**
+     * Deletes the next portion of records limited by given date.<br>
+     * The number of records to delete may be limited to batch-size by the <code>asBatch</code> flag.
+     *
+     * @param effort  the database to delete a batch from
+     * @param date    records older than this date are delete-candidates
+     * @param asBatch <code>true</code> demands deletion of configured batch-size,
+     *                <code>false</code> deletes everything
+     * @return number of records this batch deleted
+     */
+    @Transactional
+    public long delete(CleaningEffort effort, LocalDate date, boolean asBatch) {
+        long count = -1L;
+        if (effort != null) {
+            if (asBatch) {
+                count = effort.jdbcTemplate.update(effort.deleteBatchQuery, new Object[] { date, effort.deleteBatchSize });
+            } else {
+                count = effort.jdbcTemplate.update(effort.deleteAllQuery, new Object[] { date });
+            }
+        }
+        return count;
+    }
+
+    public long getCount(CleaningEffort effort, LocalDate date) {
+        long count = -1L;
+        if (effort != null) {
+            count = effort.jdbcTemplate.queryForObject(effort.countQuery, Long.class, new Object[] { date } );
+        }
+        return count;
+    }
+
+    public CleaningEffort create(String databaseName, Cleanup cleanup) {
+
+        CleaningEffort effort = null;
+
+        boolean nameValid = StringUtils.isNoneBlank(databaseName);
+        if (!nameValid) {
+            log.debug("CLEANING | Init {} - name '{}' invalid", databaseName, databaseName);
+        }
+
+        Database db = cleanup.getDatabase();
+        boolean databaseValid = false;
+        if (db == null) {
+            log.debug("CLEANING | Init {} - database config for '{}' missing", databaseName, databaseName);
+        } else {
+            databaseValid = StringUtils.isNoneBlank(db.getUrl(), db.getUsername(), db.getPassword());
+            if (!databaseValid) {
+                log.debug("CLEANING | Init {} - database config invalid:\n url='{}'\n username='{}'\n password='***", databaseName, db.getUrl(), db.getUsername());
+            }
+        }
+
+        SqlQuery query = cleanup.getSqlQuery();
+        boolean sqlQueryValid = false;
+        if (query == null) {
+            log.debug("CLEANING | Init {} - query config for '{}' missing", databaseName, databaseName);
+        } else {
+            sqlQueryValid = StringUtils.isNoneBlank(
+                    query.getCount(), query.getDeleteAll(), query.getDeleteBatch()) && query.getDeleteBatchSize()>-1;
+            if (!sqlQueryValid) {
+                log.debug("CLEANING | Init {} - query config invalid:\n count='{}'\n deleteAll='{}'\n deleteBatch='{}'\ndeleteBatchSize={}", databaseName,
+                        query.getCount(), query.getDeleteAll(), query.getDeleteBatch(), query.getDeleteBatchSize());
+            }
+        }
+
+        if (nameValid && databaseValid && sqlQueryValid) {
+            HikariDataSource hikariDataSource = null;
+            try {
+                hikariDataSource = createHikariDataSource(databaseName, db.getUrl(), db.getUsername(), db.getPassword());
+                JdbcTemplate jdbcTemplate = createJdbcTemplate(hikariDataSource);
+                effort = new CleaningEffort(databaseName,
+                        query.getCount(), query.getDeleteAll(), query.getDeleteBatch(), query.getDeleteBatchSize(), hikariDataSource, jdbcTemplate);
+            } catch (Exception e) {
+                log.debug("CLEANING | Init {} - error: '{}'", databaseName, e.getMessage());
+                if (hikariDataSource != null) {
+                    try {
+                        hikariDataSource.close();
+                    } catch (Throwable t) {
+                        log.debug("CLEANING | Init {} - error on closing datasource: '{}'", databaseName, t.getMessage());
+                    }
+                }
+            }
+        }
+
+        return effort;
+    }
+
+    public void destroy(CleaningEffort effort) {
+        JdbcTemplate jdbcTemplate = effort.getJdbcTemplate();
+        effort.setJdbcTemplate(null);
+
+        jdbcTemplate.setDataSource(null);
+
+        HikariDataSource hikariDataSource = effort.getDataSource();
+        effort.setDataSource(null);
+
+        if (hikariDataSource != null) {
+            hikariDataSource.close();
+        }
+    }
+
+    private HikariDataSource createHikariDataSource(String databaseName, String url, String username, String password) {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setJdbcUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setMaximumPoolSize(2);
+        dataSource.setMinimumIdle(0);
+        dataSource.setPoolName("cleanup-"+databaseName);
+        return dataSource;
+    }
+
+    private JdbcTemplate createJdbcTemplate(HikariDataSource hikariDataSource) {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(hikariDataSource);
+        jdbcTemplate.queryForObject("select true where 1=1", Boolean.class);
+        return jdbcTemplate;
+    }
+
+    @Data
+    @AllArgsConstructor
+    class CleaningEffort {
+
+        private final String name;
+        private final String countQuery;
+        private final String deleteAllQuery;
+        private final String deleteBatchQuery;
+        private final int deleteBatchSize;
+        private HikariDataSource dataSource;
+        private JdbcTemplate jdbcTemplate;
+    }
+}
